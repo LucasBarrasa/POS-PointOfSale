fad2c1651a10bac3e34662ce41eafc21c2a66615
Lucas Barrasa <lucasbarrasa.dev@gmail.com>
2026-04-27 20:45:04 -0300

    chore: initial multi-module project setup

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Chester"
+include(":app")
+include(":core:navigation")
+include(":core:data")
+include(":core:model")
+include(":core:network")
+include(":features:seller")
+include(":features:createorder")
+include(":features:orderdetail")
+include(":core:ui")
